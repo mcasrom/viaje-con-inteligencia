763cc8dfad0e9856c04571f5208a55cbb5f4e578
mcasrom <mcasrom@gmail.com>
2026-04-25 09:47:51 +0200

    Add force-dynamic

--- src/app/api/subscription/route.ts
+++ src/app/api/subscription/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { stripe, PLANS } from '@/lib/stripe';
 
+export const dynamic = 'force-dynamic';
+
 export async function POST(req: NextRequest) {
   try {
     const { priceId, userId } = await req.json();
