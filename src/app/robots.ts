ef2893d40a8ab2f468a271be576cd8f40ad08bd5
mcasrom <mcasrom@gmail.com>
2026-05-21 08:10:19 +0200

    feat: robots.ts dinámico Next.js - eliminar estático public/robots.txt

---
+++ src/app/robots.ts
@@ -0,0 +1,30 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/api/',
+          '/dashboard/',
+          '/admin/',
+          '/test-leaflet',
+          '/api-endpoints',
+          '/kpi',
+          '/radius',
+          '/relojes',
+          '/lead-magnet',
+          '/precio-api',
+          '/reclamaciones',
+        ],
+      },
+      {
+        userAgent: 'GPTBot',
+        disallow: '/',
+      },
+    ],
+    sitemap: 'https://www.viajeinteligencia.com/sitemap.xml',
+  }
+}
