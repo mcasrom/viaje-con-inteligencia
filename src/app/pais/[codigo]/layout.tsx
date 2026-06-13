c33b42e7b89d2c6d264457fc9c058a6d77e38900
mcasrom <mcasrom@gmail.com>
2026-06-14 01:14:52 +0200

    fix pais layout build

--- src/app/pais/[codigo]/layout.tsx
+++ src/app/pais/[codigo]/layout.tsx
@@ -1,6 +1,12 @@
+import { ReactNode } from "react";
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return children;
+}
+
 export const metadata = {
   robots: {
     index: false,
-    follow: false
-  }
-}
+    follow: false,
+  },
+};
